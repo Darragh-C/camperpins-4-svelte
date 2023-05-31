919534460c150dad0111dfed585f565be94a8bc3
Darragh Conneely <darraghc23@gmail.com>
2023-05-31 15:17:04 +0100

    weather graph

---
+++ src/lib/WeatherTrend.svelte
@@ -0,0 +1,70 @@
+<script>
+  import Chart from "svelte-frappe-charts";
+  import { onMount, afterUpdate } from "svelte";
+
+  export let weatherData = [];
+
+
+  const dateTimes = weatherData.map(data => data.dt);
+  const timePlot = dateTimes.slice(0, 10).map(timestamp => formatTimestamp(timestamp));
+  
+  const tempReadings = weatherData.slice(0, 10).map(data => data.temp);
+  const humidReadings = weatherData.slice(0, 10).map(data => data.humidity);
+  const feelsLike = weatherData.slice(0, 10).map(data => data.feels_like);
+  const clouds = weatherData.slice(0, 10).map(data => data.clouds);
+  const pressure = weatherData.slice(0, 10).map(data => data.pressure/10);
+  const windSpeed = weatherData.slice(0, 10).map(data => data.wind_speed/10);
+  const windGust = weatherData.slice(0, 10).map(data => data.wind_gust/10); 
+  
+  function formatTimestamp(timestamp) {
+    var date = new Date(timestamp * 1000);
+
+    var day = "0" + date.getDate();
+    var month = "0" + (date.getMonth() + 1);
+    var year = date.getFullYear().toString().substr(-2);
+    var hours = "0" + date.getHours();
+    var minutes = "0" + date.getMinutes();
+
+    return hours.substr(-2) + ':' + minutes.substr(-2) + ' ' + day.substr(-2) + '/' + month.substr(-2) + '/' + year;
+  }
+  
+  let data = {
+      labels: timePlot,
+      datasets: [
+          {
+              name: "Temperature",  
+              values: tempReadings
+          },
+          {
+              name: "Feels like",  
+              values: feelsLike
+          },
+          {
+              name: "Humidity",
+              values: humidReadings
+          },
+          {
+              name: "Clouds",
+              values: clouds
+          },
+          {
+              name: "Pressure/10",
+              values: pressure
+          },
+          {
+              name: "Wind speed",
+              values: windSpeed
+          },
+          {
+              name: "Wind gust",
+              values: windGust
+          }
+      ]
+  };
+
+</script>
+
+
+<h1>Graph</h1>
+<Chart {data} type="line" />
+
--- src/routes/weather/+page.svelte
+++ src/routes/weather/+page.svelte
@@ -1,14 +1,17 @@
 <script>
 
+  import ChartTemplate from "$lib/ChartTemplate.svelte";
   import MainNavigator from "$lib/MainNavigator.svelte";
-    import PinWeather from "$lib/PinWeather.svelte";
+  import PinWeather from "$lib/PinWeather.svelte";
   import TitleBar from '$lib/TitleBar.svelte';
+  import WeatherTrend from "$lib/WeatherTrend.svelte";
 
   export let data;
 
   console.log(data);
 
   const icon = data.weather.current.weather[0].icon;
+  const hourly = data.weather.hourly;
 
   const iconSrc = `https://openweathermap.org/img/wn/${icon}@2x.png`
 
@@ -26,3 +29,9 @@
   <h2 class="title is-2">{data.pin.name} weather</h2>
 
   <PinWeather weatherData={data.weather} />
+
+  <WeatherTrend weatherData={hourly} />
+<!--
+
+-->
+  
